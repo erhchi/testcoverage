fac5b00e03c21bdf602713d7d726a7802a91475f
erichaerr@outlook.com <erichaerr@outlook.com>
2018-06-25 17:11:56 -0500

    testing

---
+++ src/main/java/DummyClass.java
@@ -0,0 +1,11 @@
+public class DummyClass {
+    private int value;
+
+    public DummyClass(){
+        this.value = 5;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+}
---
+++ src/test/java/DummyClassTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DummyClassTest {
+
+    DummyClass dc = new DummyClass();
+
+    @Test
+    public void getValue() {
+        assertEquals(dc.getValue(),5);
+    }
+}
